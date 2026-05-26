91f78be4526e08c4279df1df308b31e72fc2aa65
James Elliott <jjellio@sandia.gov>
2026-05-26 10:42:26 -0600

    static linkage works

--- repos/spack_repo/builtin/packages/netcdf_c/package.py
+++ repos/spack_repo/builtin/packages/netcdf_c/package.py
@@ -53,6 +53,9 @@ class NetcdfC(CMakePackage, AutotoolsPackage):
     version("4.3.3", sha256="3f16e21bc3dfeb3973252b9addf5defb48994f84fc9c9356081f871526a680e7")
 
     with when("build_system=cmake"):
+
+        patch("change-link-interface-definition-order.patch", when="@4.9.3")
+
         # TODO: document why we need to revert https://github.com/Unidata/netcdf-c/pull/1731
         #  with the following patch:
         patch("4.8.1-win-hdf5-with-zlib.patch", when="@4.8.1:4.9.2 platform=windows")
